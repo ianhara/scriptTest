1fcf6a7d11a79e81dbba2fa24971044a53a20168
ianhara <ianhara97@gmail.com>
2024-03-03 17:13:46 -0500

    added comments and finished displayFinal fn

--- script.js
+++ script.js
@@ -1,89 +1,74 @@
-var quizTime = 120
-var startBtn = document.getElementById("startBtn")
-
-startBtn.addEventListener("click", startTime)
-var questionIndex = 0
-//set up questions as objects in an array 
+var quizTime = 120;
+var startBtn = document.getElementById("startBtn");
+//if start button clicked run startTime
+startBtn.addEventListener("click", startTime);
+var questionIndex = 0;
+//set up questions as objects in an array
 var questions = [
-    {
-        questionText: "what line of code is used to link a local jscript file in html" ,
-        answers: ["<jscript>a href = /jscript </jscript>", "<script src = script,js</script>", "<script src = script.js</script>", "link script = true"],
-        key: "option3"
-    },
-    {
-        questionText: "question2" ,
-        answers: ["option1", "option2", "option3", "option4"],
-        key: "option2"
-    },
-    {
-        questionText: "question3" ,
-        answers: ["option1", "option2", "option3", "option4"],
-        key: "option1"
+  {
+    questionText:
+      "what line of code is used to link a local jscript file in html",
+    answers: [
+      "<jscript>a href = /jscript </jscript>",
+      "<script src = script,js</script>",
+      "<script src = script.js</script>",
+      "link script = true",
+    ],
+    key: "option3",
+  },
+  {
+    questionText: "question2",
+    answers: ["option1", "option2", "option3", "option4"],
+    key: "option2",
+  },
+  {
+    questionText: "question3",
+    answers: ["option1", "option2", "option3", "option4"],
+    key: "option1",
+  },
+];
+
+function showQuestion() {
+  //element for answer buttons
+  var answersEl = document.getElementById("answers");
+  //element for the question text
+  var questionEl = document.getElementById("question");
+  //current question from array
+  var currentQuestion = questions[questionIndex];
+  // clearing the page of any leftover answers
+  answersEl.innerHTML = " ";
+
+  //display question of question index
+  questionEl.textContent = currentQuestion.questionText;
+
+  //for loop to iterate through answers
+  for (var i = 0; i < currentQuestion.answers.length; i++) {
+    var button = document.createElement("button");
+    //create button with content of answer at i
+    button.textContent = currentQuestion.answers[i];
+    //append to be displayed
+    answersEl.appendChild(button);
+  }
+}
+//starts timer and stops upon 0 time left
+function startTimer() {
+  var displayTimer = document.getElementById("timer");
+  var interval = setInterval(function () {
+    quizTime--;
+    displayTimer.textContent = "time left: " + quizTime + "seconds";
+    if (quizTime <= 0) {
+      clearInterval(interval);
+      displayFinal();
     }
-
-]
-
-
-function showQuestion(){
-    //element for answer buttons
-    var answersEl = document.getElementById("answers")
-    //element for the question text
-    var questionEl = document.getElementById("question")
-    //current question from array
-    var currentQuestion = questions[questionIndex]
-     // clearing the page of any leftover answers
-     answersEl.innerHTML=" "
-
-    //display question of question index
-    questionEl.textContent = currentQuestion.questionText
-
-    for(var i =0; i<currentQuestion.answers.length; i++){
-        
-  var button = document.createElement("button");
-
-  
-  button.textContent = currentQuestion.answers[i]
-  
-  
-  
-  answersEl.appendChild(button);
-    }
+  }, 1000);
 }
-
-function startTimer(){
-    var displayTimer = document.getElementById("timer")
-    var interval = setInterval(function(){
-        quizTime--
-        displayTimer.textContent = "time left: " + quizTime + "seconds"
-        if (quizTime <= 0){
-            clearInterval(interval)
-            displayFinal()
-        }
-    }, 1000)
+//runs showQuestion and Start timer
+function startTime() {
+  showQuestion();
+  startTimer();
 }
-
-
-
-function startTime(){
-    showQuestion()
-    startTimer()
-    console.log(questions[questionIndex].questionText)
-    
+//once display final is triggered we want to display score as time left
+function displayFinal() {
+  var final = document.getElementById("final");
+  final.textContent = "final score: " + quizTime;
 }
-
-function displayFinal(){
-    var final = document.getElementById("final")
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
